aabf809dc269b872096cc9eadee977812914088c
AzatBaisynov <azat.baisynov@gmail.com>
2022-04-07 21:55:02 +0600

    init

---
+++ script.js
@@ -0,0 +1,65 @@
+// + - / * % **
+
+// console.log(5 + 5)
+// console.log(5 - 5)
+// console.log(5 * 5)
+// console.log(5 / 5)
+// console.log(10 % 7)
+// console.log(2**3) // 2 * 2 * 2
+
+// let a = 20
+
+// a++ // инкремент
+// a--  // декремент
+// a = a + 10
+
+// a = a - 20 // 0
+// a+=10
+// a-=20
+// a*=20
+// a/=20
+
+// const a = 2.5
+//
+// const b = Math.round(a)  // Округление до близжайшего значения
+// const c = Math.floor(a)  // Округление до минимального значения
+// const d = Math.ceil(a)   // Округление до максимального значения
+
+// console.log(Math.pow(2, 3))
+// console.log(Math.sqrt(9))
+// Math.round(Math.random() * 100)
+
+// let num = "5"
+// num = Number(num)
+// num = +num
+// console.log(num + 5)
+
+// 9. Вам даны a="10" и b="99". Сложите переменные a и b и
+// присвойте результат переменной result и выведите её в консоль.
+//
+// let a = "10" // string
+// let b = "99" // string
+// // "hello " + "world" конкатенация
+// const result = Number(a) + Number(b)
+// const b = parseInt(a)
+//
+// console.log(result)
+
+// 8. Вам даны переменные a=17 и b=10. Отнимите от a переменную b и результат
+// присвойте переменной c. Затем создайте переменную d, присвойте ей значение.
+// Сложите переменные c и d, а результат запишите в переменную result.
+// Выведите в консоль значение переменной result.
+
+// const a = 17
+// const b = 10
+// const c = a - b
+// const d = 50
+// const result = c + d
+// console.log(result)
+
+// let num = 5
+// num = String(num)
+// num = num.toString()
+// num = num + ""
+// console.log(typeof num)
+
